fe5506d170b0c589537753ca7c3700485fe58a62
Ahmet <ahmetcanlyev@gmail.com>
2024-07-18 15:25:26 +0500

    changes

--- src/views/home/screens/WallScreen.jsx
+++ src/views/home/screens/WallScreen.jsx
@@ -1,75 +1,41 @@
 import React from "react";
 import PaginationButton from "src/components/buttons/PaginationButton";
 
+const data = [
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+  {
+    text: "PLACEHOLDER",
+  },
+];
+
 const WallScreen = () => {
   return (
     <div className="wallscreen screen">
       <div className="wallscreen__container">
         <div className="wallscreen__container-row">
-          <div className="wallscreen__frame">
-            <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-1.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-          <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-2.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-          <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-3.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-          <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-4.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-            <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-1.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-          <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-2.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-          <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-3.png" alt="app-screen-frame" />
-            </div>
-          </div>
-          <div className="wallscreen__frame">
-          <div className="wallscreen__frame-text">
-              PLACEHOLDER
-            </div>
-            <div className="wallscreen__frame-bg">
-              <img src="/images/wall/frame-4.png" alt="app-screen-frame" />
-            </div>
-          </div>
+          {data.map((item, i) => (
+            <WallItem key={i} text={item.text} />
+          ))}
         </div>
         <div className="wallscreen__container-pagination">
           <PaginationButton text={"1"} />
@@ -86,6 +52,17 @@ const WallScreen = () => {
           <PaginationButton text={"12"} />
           <PaginationButton text={"13"} />
         </div>
+      </div>
+    </div>
+  );
+};
+
+const WallItem = ({ text }) => {
+  return (
+    <div className="wallscreen__frame">
+      <div className="wallscreen__frame-text">{text}</div>
+      <div className="wallscreen__frame-bg">
+        <img src="/images/wall/frame-4.png" alt="app-screen-frame" />
       </div>
     </div>
   );
