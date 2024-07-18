d315bdb5ef8c6462993b2e1d652e4bcb4d27b27a
Ahmet <ahmetcanlyev@gmail.com>
2024-07-18 17:09:33 +0500

    responsvie

--- src/views/home/screens/BoongaScreen.jsx
+++ src/views/home/screens/BoongaScreen.jsx
@@ -5,6 +5,91 @@ import ArrowButton, {
 import Button, { BUTTON_VARIANTS } from "src/components/buttons/Button";
 import Textarea from "src/components/textarea/Textarea";
 
+const stickers = [
+  {
+    img: "/images/boonga/stickers/alien.png",
+    alt: "alien",
+  },
+  {
+    img: "/images/boonga/stickers/alien-2.png",
+    alt: "alien",
+  },
+  {
+    img: "/images/boonga/stickers/anteater.png",
+    alt: "anteater",
+  },
+  {
+    img: "/images/boonga/stickers/attack.png",
+    alt: "attack",
+  },
+  {
+    img: "/images/boonga/stickers/bang.png",
+    alt: "bang",
+  },
+  {
+    img: "/images/boonga/stickers/best-friend.png",
+    alt: "best-friend",
+  },
+  {
+    img: "/images/boonga/stickers/bit-bang-logo.png",
+    alt: "bit-bang-logo",
+  },
+  {
+    img: "/images/boonga/stickers/bitcoin-logo.png",
+    alt: "bitcoin-logo",
+  },
+  {
+    img: "/images/boonga/stickers/bow.png",
+    alt: "bow",
+  },
+  {
+    img: "/images/boonga/stickers/bring-stone.png",
+    alt: "bring-stone",
+  },
+];
+const items = [
+  {
+    img: "/images/boonga/items/bit-bang-necklace.png",
+    alt: "bit-bang-necklace",
+  },
+  {
+    img: "/images/boonga/items/bit-bang-necklace-front-view.png",
+    alt: "bit-bang-necklace-front-view",
+  },
+  {
+    img: "/images/boonga/items/caveman-beard.png",
+    alt: "caveman-beard",
+  },
+  {
+    img: "/images/boonga/items/caveman-beard-front-view.png",
+    alt: "caveman-beard-front-view",
+  },
+  {
+    img: "/images/boonga/items/caveman-head.png",
+    alt: "caveman-head",
+  },
+  {
+    img: "/images/boonga/items/caveman-head-front.png",
+    alt: "caveman-head-front",
+  },
+  {
+    img: "/images/boonga/items/rock-cap.png",
+    alt: "rock-cap",
+  },
+  {
+    img: "/images/boonga/items/rock-cap-front-view.png",
+    alt: "rock-cap-front-view",
+  },
+  {
+    img: "/images/boonga/items/rockulus-vr.png",
+    alt: "rockulus-vr",
+  },
+  {
+    img: "/images/boonga/items/rockulus-vr---front-view.png",
+    alt: "rockulus-vr---front-view",
+  },
+];
+
 const BoongaScreen = () => {
   return (
     <div className="boongascreen screen">
@@ -27,10 +112,11 @@ const BoongaScreen = () => {
               className="boongascreen__slider-arrow"
             />
             <div className="boongascreen__slider-row">
-              <div className="boongascreen__slider-item"></div>
-              <div className="boongascreen__slider-item"></div>
-              <div className="boongascreen__slider-item"></div>
-              <div className="boongascreen__slider-item"></div>
+              {stickers.map((item, i) => (
+                <div className="boongascreen__slider-item" key={i}>
+                  <img src={item.img} alt={item.alt} />
+                </div>
+              ))}
             </div>
             <ArrowButton
               direction={ARROW_DIRECTIONS.RIGHT}
@@ -46,10 +132,11 @@ const BoongaScreen = () => {
               className="boongascreen__slider-arrow"
             />
             <div className="boongascreen__slider-row">
-              <div className="boongascreen__slider-item"></div>
-              <div className="boongascreen__slider-item"></div>
-              <div className="boongascreen__slider-item"></div>
-              <div className="boongascreen__slider-item"></div>
+              {items.map((item, i) => (
+                <div className="boongascreen__slider-item" key={i}>
+                  <img src={item.img} alt={item.alt} />
+                </div>
+              ))}
             </div>
             <ArrowButton
               direction={ARROW_DIRECTIONS.RIGHT}
@@ -61,9 +148,9 @@ const BoongaScreen = () => {
       <div className="boongascreen__content">
         <div className="boongascreen__content-text">
           <span className="boongascreen__content-text-left">HUMAN</span>
-          <span className="boongascreen__content-ico">
+          <button className="boongascreen__content-ico">
             <img src="/images/components/arrows.png" alt="arrow" />
-          </span>
+          </button>
           <span className="boongascreen__content-text-right">CAVEMAN</span>
         </div>
         <div className="boongascreen__content-row">
