0032d2677fc7b57c8bfb6d6ede80f8b037efe47a
Hyzyr <avezberdiyew@gmail.com>
2024-07-21 17:09:51 +0500

    sahdow bg

--- src/views/home/HomeBg.jsx
+++ src/views/home/HomeBg.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const HomeBg = () => {
   const ref = React.useRef();
@@ -8,8 +8,29 @@ const HomeBg = () => {
       <BgScreen bgVideo="/videos/left_background.webm" />
       <BgScreen bgVideo="/videos/mid_background.webm" />
       <BgScreen bgVideo="/videos/right_background.webm" />
+      <div className="home__bg-stone">
+        <img src="/images/front-stone.png" alt="fixed stone" />
+      </div>
+      <ShadowLight />
     </div>
   );
+};
+
+const ShadowLight = () => {
+  useEffect(() => {
+    const listener = (e) => {
+      window.event = e;
+      console.log(e.clientX, e.clientY);
+      document.documentElement.style.setProperty('--mouseX', `${e.clientX}px`);
+      document.documentElement.style.setProperty('--mouseY', `${e.clientY}px`);
+    };
+
+    document.body.addEventListener('mousemove', listener);
+
+    return () => document.body.removeEventListener('mousemove', listener);
+  }, []);
+
+  return <div className="home__bg-shadow" />;
 };
 
 const BgScreen = ({ bgVideo = '', className = '' }) => {
