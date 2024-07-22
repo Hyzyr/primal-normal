c10ad7c0008893dd508d7d2df811b23ff1e60d61
Ahmet <ahmetcanlyev@gmail.com>
2024-07-22 18:34:36 +0500

    add constant stikers

--- src/views/home/screens/BoongaScreen/constants.js
+++ src/views/home/screens/BoongaScreen/constants.js
@@ -1,243 +1,570 @@
 export const stickers = [
   {
-    img: '/images/boonga/stickers/alien.png',
-    alt: 'alien',
+    img: "/images/boonga/stickers/alien.png",
+    title: "alien.png",
   },
   {
-    img: '/images/boonga/stickers/alien-2.png',
-    alt: 'alien',
+    img: "/images/boonga/stickers/alien-2.png",
+    title: "alien-2.png",
   },
   {
-    img: '/images/boonga/stickers/anteater.png',
-    alt: 'anteater',
+    img: "/images/boonga/stickers/anteater.png",
+    title: "anteater.png",
   },
   {
-    img: '/images/boonga/stickers/attack.png',
-    alt: 'attack',
+    img: "/images/boonga/stickers/attack.png",
+    title: "attack.png",
   },
   {
-    img: '/images/boonga/stickers/bang.png',
-    alt: 'bang',
+    img: "/images/boonga/stickers/bang.png",
+    title: "bang.png",
   },
   {
-    img: '/images/boonga/stickers/best-friend.png',
-    alt: 'best-friend',
+    img: "/images/boonga/stickers/best-friend.png",
+    title: "best-friend.png",
   },
   {
-    img: '/images/boonga/stickers/bit-bang-logo.png',
-    alt: 'bit-bang-logo',
+    img: "/images/boonga/stickers/bit-bang-logo.png",
+    title: "bit-bang-logo.png",
   },
   {
-    img: '/images/boonga/stickers/bitcoin-logo.png',
-    alt: 'bitcoin-logo',
+    img: "/images/boonga/stickers/bitcoin-logo.png",
+    title: "bitcoin-logo.png",
   },
   {
-    img: '/images/boonga/stickers/bow.png',
-    alt: 'bow',
+    img: "/images/boonga/stickers/bow.png",
+    title: "bow.png",
   },
   {
-    img: '/images/boonga/stickers/bring-stone.png',
-    alt: 'bring-stone',
+    img: "/images/boonga/stickers/bring-stone.png",
+    title: "bring-stone.png",
+  },
+  {
+    img: "/images/boonga/stickers/buffaloed.png",
+    title: "buffaloed.png",
+  },
+  {
+    img: "/images/boonga/stickers/burn-down.png",
+    title: "burn-down.png",
+  },
+  {
+    img: "/images/boonga/stickers/calm-pepe.png",
+    title: "calm-pepe.png",
+  },
+  {
+    img: "/images/boonga/stickers/carve-money.png",
+    title: "carve-money.png",
+  },
+  {
+    img: "/images/boonga/stickers/carve-stone.png",
+    title: "carve-stone.png",
+  },
+  {
+    img: "/images/boonga/stickers/charged-bonk.png",
+    title: "charged-bonk.png",
+  },
+  {
+    img: "/images/boonga/stickers/cheer.png",
+    title: "cheer.png",
+  },
+  {
+    img: "/images/boonga/stickers/classic-finger.png",
+    title: "classic-finger.png",
+  },
+  {
+    img: "/images/boonga/stickers/cuckold.png",
+    title: "cuckold.png",
+  },
+  {
+    img: "/images/boonga/stickers/currency.png",
+    title: "currency.png",
+  },
+  {
+    img: "/images/boonga/stickers/dance.png",
+    title: "dance.png",
+  },
+  {
+    img: "/images/boonga/stickers/dance-2.png",
+    title: "dance-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/dances.png",
+    title: "dances.png",
+  },
+  {
+    img: "/images/boonga/stickers/ded.png",
+    title: "ded.png",
+  },
+  {
+    img: "/images/boonga/stickers/discovery.png",
+    title: "discovery.png",
+  },
+  {
+    img: "/images/boonga/stickers/doggo.png",
+    title: "doggo.png",
+  },
+  {
+    img: "/images/boonga/stickers/extinction.png",
+    title: "extinction.png",
+  },
+  {
+    img: "/images/boonga/stickers/extinction-2.png",
+    title: "extinction-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/extinction-3.png",
+    title: "extinction-3.png",
+  },
+  {
+    img: "/images/boonga/stickers/extinction-4.png",
+    title: "extinction-4.png",
+  },
+  {
+    img: "/images/boonga/stickers/fire.png",
+    title: "fire.png",
+  },
+  {
+    img: "/images/boonga/stickers/flash.png",
+    title: "flash.png",
+  },
+  {
+    img: "/images/boonga/stickers/freaky.png",
+    title: "freaky.png",
+  },
+  {
+    img: "/images/boonga/stickers/gator-ate.png",
+    title: "gator-ate.png",
+  },
+  {
+    img: "/images/boonga/stickers/giant-shiba.png",
+    title: "giant-shiba.png",
+  },
+  {
+    img: "/images/boonga/stickers/gojira.png",
+    title: "gojira.png",
+  },
+  {
+    img: "/images/boonga/stickers/hole-ready.png",
+    title: "hole-ready.png",
+  },
+  {
+    img: "/images/boonga/stickers/horsey.png",
+    title: "horsey.png",
+  },
+  {
+    img: "/images/boonga/stickers/hunt.png",
+    title: "hunt.png",
+  },
+  {
+    img: "/images/boonga/stickers/invisible-horse.png",
+    title: "invisible-horse.png",
+  },
+  {
+    img: "/images/boonga/stickers/knight-stick.png",
+    title: "knight-stick.png",
+  },
+  {
+    img: "/images/boonga/stickers/kong.png",
+    title: "kong.png",
+  },
+  {
+    img: "/images/boonga/stickers/lion.png",
+    title: "lion.png",
+  },
+  {
+    img: "/images/boonga/stickers/lioness-ambush.png",
+    title: "lioness-ambush.png",
+  },
+  {
+    img: "/images/boonga/stickers/livestock.png",
+    title: "livestock.png",
+  },
+  {
+    img: "/images/boonga/stickers/moby-dick.png",
+    title: "moby-dick.png",
+  },
+  {
+    img: "/images/boonga/stickers/moses.png",
+    title: "moses.png",
+  },
+  {
+    img: "/images/boonga/stickers/moses-2.png",
+    title: "moses-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/mountain.png",
+    title: "mountain.png",
+  },
+  {
+    img: "/images/boonga/stickers/mountain-2.png",
+    title: "mountain-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/no-s3x-sign.png",
+    title: "no-s3x-sign.png",
+  },
+  {
+    img: "/images/boonga/stickers/nuke.png",
+    title: "nuke.png",
+  },
+  {
+    img: "/images/boonga/stickers/pepe-giant.png",
+    title: "pepe-giant.png",
+  },
+  {
+    img: "/images/boonga/stickers/pepe-relic.png",
+    title: "pepe-relic.png",
+  },
+  {
+    img: "/images/boonga/stickers/pooped.png",
+    title: "pooped.png",
+  },
+  {
+    img: "/images/boonga/stickers/puppets.png",
+    title: "puppets.png",
+  },
+  {
+    img: "/images/boonga/stickers/ranger.png",
+    title: "ranger.png",
+  },
+  {
+    img: "/images/boonga/stickers/rogue-snake.png",
+    title: "rogue-snake.png",
+  },
+  {
+    img: "/images/boonga/stickers/run.png",
+    title: "run.png",
+  },
+  {
+    img: "/images/boonga/stickers/running-bonk.png",
+    title: "running-bonk.png",
+  },
+  {
+    img: "/images/boonga/stickers/s3x.png",
+    title: "s3x.png",
+  },
+  {
+    img: "/images/boonga/stickers/sea-weed.png",
+    title: "sea-weed.png",
+  },
+  {
+    img: "/images/boonga/stickers/shiba-pack.png",
+    title: "shiba-pack.png",
+  },
+  {
+    img: "/images/boonga/stickers/shoot-arrow.png",
+    title: "shoot-arrow.png",
+  },
+  {
+    img: "/images/boonga/stickers/shout.png",
+    title: "shout.png",
+  },
+  {
+    img: "/images/boonga/stickers/show.png",
+    title: "show.png",
+  },
+  {
+    img: "/images/boonga/stickers/sling-rock.png",
+    title: "sling-rock.png",
+  },
+  {
+    img: "/images/boonga/stickers/sneaky.png",
+    title: "sneaky.png",
+  },
+  {
+    img: "/images/boonga/stickers/spear.png",
+    title: "spear.png",
+  },
+  {
+    img: "/images/boonga/stickers/spear-2.png",
+    title: "spear-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/spear-3.png",
+    title: "spear-3.png",
+  },
+  {
+    img: "/images/boonga/stickers/spear-4(1).png",
+    title: "spear-4(1).png",
+  },
+  {
+    img: "/images/boonga/stickers/spear-4.png",
+    title: "spear-4.png",
+  },
+  {
+    img: "/images/boonga/stickers/spear-attack.png",
+    title: "spear-attack.png",
+  },
+  {
+    img: "/images/boonga/stickers/stone.png",
+    title: "stone.png",
+  },
+  {
+    img: "/images/boonga/stickers/stonehenge.png",
+    title: "stonehenge.png",
+  },
+  {
+    img: "/images/boonga/stickers/talking-to-stone.png",
+    title: "talking-to-stone.png",
+  },
+  {
+    img: "/images/boonga/stickers/tapir.png",
+    title: "tapir.png",
+  },
+  {
+    img: "/images/boonga/stickers/the-thinker.png",
+    title: "the-thinker.png",
+  },
+  {
+    img: "/images/boonga/stickers/trading.png",
+    title: "trading.png",
+  },
+  {
+    img: "/images/boonga/stickers/trident.png",
+    title: "trident.png",
+  },
+  {
+    img: "/images/boonga/stickers/trident-2.png",
+    title: "trident-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/tripped.png",
+    title: "tripped.png",
+  },
+  {
+    img: "/images/boonga/stickers/turtle.png",
+    title: "turtle.png",
+  },
+  {
+    img: "/images/boonga/stickers/twerk.png",
+    title: "twerk.png",
+  },
+  {
+    img: "/images/boonga/stickers/ufo.png",
+    title: "ufo.png",
+  },
+  {
+    img: "/images/boonga/stickers/ufo-2.png",
+    title: "ufo-2.png",
+  },
+  {
+    img: "/images/boonga/stickers/ufo-3.png",
+    title: "ufo-3.png",
+  },
+  {
+    img: "/images/boonga/stickers/wave.png",
+    title: "wave.png",
+  },
+  {
+    img: "/images/boonga/stickers/wave-conqueror.png",
+    title: "wave-conqueror.png",
+  },
+  {
+    img: "/images/boonga/stickers/world-peace.png",
+    title: "world-peace.png",
+  },
+  {
+    img: "/images/boonga/stickers/zen.png",
+    title: "zen.png",
   },
 ];
 export const items = [
   {
-    img: '/images/boonga/items/bit-bang-necklace.png',
-    alt: 'bit-bang-necklace',
+    img: "/images/boonga/items/bit-bang-necklace.png",
+    alt: "bit-bang-necklace",
   },
   {
-    img: '/images/boonga/items/bit-bang-necklace-front-view.png',
-    alt: 'bit-bang-necklace-front-view',
+    img: "/images/boonga/items/bit-bang-necklace-front-view.png",
+    alt: "bit-bang-necklace-front-view",
   },
   {
-    img: '/images/boonga/items/caveman-beard.png',
-    alt: 'caveman-beard',
+    img: "/images/boonga/items/caveman-beard.png",
+    alt: "caveman-beard",
   },
   {
-    img: '/images/boonga/items/caveman-beard-front-view.png',
-    alt: 'caveman-beard-front-view',
+    img: "/images/boonga/items/caveman-beard-front-view.png",
+    alt: "caveman-beard-front-view",
   },
   {
-    img: '/images/boonga/items/caveman-head.png',
-    alt: 'caveman-head',
+    img: "/images/boonga/items/caveman-head.png",
+    alt: "caveman-head",
   },
   {
-    img: '/images/boonga/items/caveman-head-front.png',
-    alt: 'caveman-head-front',
+    img: "/images/boonga/items/caveman-head-front.png",
+    alt: "caveman-head-front",
   },
   {
-    img: '/images/boonga/items/rock-cap.png',
-    alt: 'rock-cap',
+    img: "/images/boonga/items/rock-cap.png",
+    alt: "rock-cap",
   },
   {
-    img: '/images/boonga/items/rock-cap-front-view.png',
-    alt: 'rock-cap-front-view',
+    img: "/images/boonga/items/rock-cap-front-view.png",
+    alt: "rock-cap-front-view",
   },
   {
-    img: '/images/boonga/items/rockulus-vr.png',
-    alt: 'rockulus-vr',
+    img: "/images/boonga/items/rockulus-vr.png",
+    alt: "rockulus-vr",
   },
   {
-    img: '/images/boonga/items/rockulus-vr---front-view.png',
-    alt: 'rockulus-vr---front-view',
+    img: "/images/boonga/items/rockulus-vr---front-view.png",
+    alt: "rockulus-vr---front-view",
   },
   {
-    img: '/images/boonga/items/rockulus-vr.png',
-    alt: 'rockulus-vr',
+    img: "/images/boonga/items/rockulus-vr.png",
+    alt: "rockulus-vr",
   },
   {
-    img: '/images/boonga/items/rockulus-vr---front-view.png',
-    alt: 'rockulus-vr---front-view',
+    img: "/images/boonga/items/rockulus-vr---front-view.png",
+    alt: "rockulus-vr---front-view",
   },
 ];
 export const chars = [
   {
-    keyCode: 'Primal Nomad',
-    keyValue: '⚛',
+    keyCode: "Primal Nomad",
+    keyValue: "⚛",
   },
   {
-    keyCode: 'a',
-    keyValue: 'ඞ',
+    keyCode: "a",
+    keyValue: "ඞ",
   },
   {
-    keyCode: 'b',
-    keyValue: 'Ꙭ',
+    keyCode: "b",
+    keyValue: "Ꙭ",
   },
   {
-    keyCode: 'c',
-    keyValue: '𓁲',
+    keyCode: "c",
+    keyValue: "𓁲",
   },
   {
-    keyCode: 'd',
-    keyValue: '𓁤',
+    keyCode: "d",
+    keyValue: "𓁤",
   },
   {
-    keyCode: 'e',
-    keyValue: '𖡆',
+    keyCode: "e",
+    keyValue: "𖡆",
   },
   {
-    keyCode: 'f',
-    keyValue: '𓁑',
+    keyCode: "f",
+    keyValue: "𓁑",
   },
   {
-    keyCode: 'g',
-    keyValue: '𓁷',
+    keyCode: "g",
+    keyValue: "𓁷",
   },
   {
-    keyCode: 'h',
-    keyValue: '෴',
+    keyCode: "h",
+    keyValue: "෴",
   },
   {
-    keyCode: 'i',
-    keyValue: '𖨪',
+    keyCode: "i",
+    keyValue: "𖨪",
   },
   {
-    keyCode: 'j',
-    keyValue: '𓁒',
+    keyCode: "j",
+    keyValue: "𓁒",
   },
   {
-    keyCode: 'k',
-    keyValue: '𓀑',
+    keyCode: "k",
+    keyValue: "𓀑",
   },
   {
-    keyCode: 'l',
-    keyValue: '𓁄',
+    keyCode: "l",
+    keyValue: "𓁄",
   },
   {
-    keyCode: 'm',
-    keyValue: '𓁔',
+    keyCode: "m",
+    keyValue: "𓁔",
   },
   {
-    keyCode: 'n',
-    keyValue: '𓆍',
+    keyCode: "n",
+    keyValue: "𓆍",
   },
   {
-    keyCode: 'o',
-    keyValue: '𖧀',
+    keyCode: "o",
+    keyValue: "𖧀",
   },
   {
-    keyCode: 'p',
-    keyValue: 'ᙹ',
+    keyCode: "p",
+    keyValue: "ᙹ",
   },
   {
-    keyCode: 'q',
-    keyValue: '𐁃',
+    keyCode: "q",
+    keyValue: "𐁃",
   },
   {
-    keyCode: 'r',
-    keyValue: '𓀐',
+    keyCode: "r",
+    keyValue: "𓀐",
   },
   {
-    keyCode: 's',
-    keyValue: '𓁐',
+    keyCode: "s",
+    keyValue: "𓁐",
   },
   {
-    keyCode: 't',
-    keyValue: '〠',
+    keyCode: "t",
+    keyValue: "〠",
   },
   {
-    keyCode: 'u',
-    keyValue: '𓀡',
+    keyCode: "u",
+    keyValue: "𓀡",
   },
   {
-    keyCode: 'v',
-    keyValue: '𓌐',
+    keyCode: "v",
+    keyValue: "𓌐",
   },
   {
-    keyCode: 'w',
-    keyValue: '𓀬',
+    keyCode: "w",
+    keyValue: "𓀬",
   },
   {
-    keyCode: 'x',
-    keyValue: '𓃻',
+    keyCode: "x",
+    keyValue: "𓃻",
   },
   {
-    keyCode: 'y',
-    keyValue: '𓂒',
+    keyCode: "y",
+    keyValue: "𓂒",
   },
   {
-    keyCode: 'z',
-    keyValue: '𓃰',
+    keyCode: "z",
+    keyValue: "𓃰",
   },
   {
-    keyCode: '1',
-    keyValue: '𖡉',
+    keyCode: "1",
+    keyValue: "𖡉",
   },
   {
-    keyCode: '2',
-    keyValue: '𓆐',
+    keyCode: "2",
+    keyValue: "𓆐",
   },
   {
-    keyCode: '3',
-    keyValue: '𓅱',
+    keyCode: "3",
+    keyValue: "𓅱",
   },
   {
-    keyCode: '4',
-    keyValue: '𓃷',
+    keyCode: "4",
+    keyValue: "𓃷",
   },
   {
-    keyCode: '5',
-    keyValue: '𓃠',
+    keyCode: "5",
+    keyValue: "𓃠",
   },
   {
-    keyCode: '6',
-    keyValue: '𓃓',
+    keyCode: "6",
+    keyValue: "𓃓",
   },
   {
-    keyCode: '7',
-    keyValue: '🦣',
+    keyCode: "7",
+    keyValue: "🦣",
   },
   {
-    keyCode: '8',
-    keyValue: '🦕',
+    keyCode: "8",
+    keyValue: "🦕",
   },
   {
-    keyCode: '9',
-    keyValue: '🦖',
+    keyCode: "9",
+    keyValue: "🦖",
   },
   {
-    keyCode: '0',
-    keyValue: '❂',
+    keyCode: "0",
+    keyValue: "❂",
   },
-
 ];
