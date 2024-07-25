f069b6753f84243b5b550cedd147d8f6eacdf7bf
Hyzyr <avezberdiyew@gmail.com>
2024-07-25 18:27:42 +0500

    hide unused code from editor hook

--- src/views/home/screens/BoongaScreen/hooks/useEditor.js
+++ src/views/home/screens/BoongaScreen/hooks/useEditor.js
@@ -18,68 +18,68 @@ const useEditor = ({ wrapperRef, imageFile }) => {
     };
   };
 
-  useEffect(() => {
-    if (!editor || !fabric) {
-      return;
-    }
-    console.log('add listeners');
-    if (cropImage) {
-      editor.canvas.__eventListeners = {};
-      return;
-    }
+  // useEffect(() => {
+  //   if (!editor || !fabric) {
+  //     return;
+  //   }
+  //   console.log('add listeners');
+  //   if (cropImage) {
+  //     editor.canvas.__eventListeners = {};
+  //     return;
+  //   }
 
-    if (!editor.canvas.__eventListeners['mouse:wheel']) {
-      console.log('mouse:wheel');
-      editor.canvas.on('mouse:wheel', function (opt) {
-        var delta = opt.e.deltaY;
-        var zoom = editor.canvas.getZoom();
-        zoom *= 0.999 ** delta;
-        if (zoom > 20) zoom = 20;
-        if (zoom < 0.01) zoom = 0.01;
-        editor.canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-        opt.e.preventDefault();
-        opt.e.stopPropagation();
-      });
-    }
+  //   if (!editor.canvas.__eventListeners['mouse:wheel']) {
+  //     console.log('mouse:wheel');
+  //     editor.canvas.on('mouse:wheel', function (opt) {
+  //       var delta = opt.e.deltaY;
+  //       var zoom = editor.canvas.getZoom();
+  //       zoom *= 0.999 ** delta;
+  //       if (zoom > 20) zoom = 20;
+  //       if (zoom < 0.01) zoom = 0.01;
+  //       editor.canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  //       opt.e.preventDefault();
+  //       opt.e.stopPropagation();
+  //     });
+  //   }
 
-    if (!editor.canvas.__eventListeners['mouse:down']) {
-      editor.canvas.on('mouse:down', function (opt) {
-        var evt = opt.e;
-        if (evt.ctrlKey === true) {
-          this.isDragging = true;
-          this.selection = false;
-          this.lastPosX = evt.clientX;
-          this.lastPosY = evt.clientY;
-        }
-      });
-    }
+  //   if (!editor.canvas.__eventListeners['mouse:down']) {
+  //     editor.canvas.on('mouse:down', function (opt) {
+  //       var evt = opt.e;
+  //       if (evt.ctrlKey === true) {
+  //         this.isDragging = true;
+  //         this.selection = false;
+  //         this.lastPosX = evt.clientX;
+  //         this.lastPosY = evt.clientY;
+  //       }
+  //     });
+  //   }
 
-    if (!editor.canvas.__eventListeners['mouse:move']) {
-      editor.canvas.on('mouse:move', function (opt) {
-        if (this.isDragging) {
-          var e = opt.e;
-          var vpt = this.viewportTransform;
-          vpt[4] += e.clientX - this.lastPosX;
-          vpt[5] += e.clientY - this.lastPosY;
-          this.requestRenderAll();
-          this.lastPosX = e.clientX;
-          this.lastPosY = e.clientY;
-        }
-      });
-    }
+  //   if (!editor.canvas.__eventListeners['mouse:move']) {
+  //     editor.canvas.on('mouse:move', function (opt) {
+  //       if (this.isDragging) {
+  //         var e = opt.e;
+  //         var vpt = this.viewportTransform;
+  //         vpt[4] += e.clientX - this.lastPosX;
+  //         vpt[5] += e.clientY - this.lastPosY;
+  //         this.requestRenderAll();
+  //         this.lastPosX = e.clientX;
+  //         this.lastPosY = e.clientY;
+  //       }
+  //     });
+  //   }
 
-    if (!editor.canvas.__eventListeners['mouse:up']) {
-      editor.canvas.on('mouse:up', function (opt) {
-        // on mouse up we want to recalculate new interaction
-        // for all objects, so we call setViewportTransform
-        this.setViewportTransform(this.viewportTransform);
-        this.isDragging = false;
-        this.selection = true;
-      });
-    }
+  //   if (!editor.canvas.__eventListeners['mouse:up']) {
+  //     editor.canvas.on('mouse:up', function (opt) {
+  //       // on mouse up we want to recalculate new interaction
+  //       // for all objects, so we call setViewportTransform
+  //       this.setViewportTransform(this.viewportTransform);
+  //       this.isDragging = false;
+  //       this.selection = true;
+  //     });
+  //   }
 
-    editor.canvas.renderAll();
-  }, [editor]);
+  //   editor.canvas.renderAll();
+  // }, [editor]);
 
   // const addBackground = () => {
   //   if (!editor || !fabric) return;
@@ -134,52 +134,52 @@ const useEditor = ({ wrapperRef, imageFile }) => {
     }
   }, [imageFile]);
 
-  const toggleSize = () => {
-    editor.canvas.freeDrawingBrush.width === 12
-      ? (editor.canvas.freeDrawingBrush.width = 5)
-      : (editor.canvas.freeDrawingBrush.width = 12);
-  };
+  // const toggleSize = () => {
+  //   editor.canvas.freeDrawingBrush.width === 12
+  //     ? (editor.canvas.freeDrawingBrush.width = 5)
+  //     : (editor.canvas.freeDrawingBrush.width = 12);
+  // };
 
-  useEffect(() => {
-    if (!editor || !fabric) {
-      return;
-    }
-    editor.canvas.freeDrawingBrush.color = color;
-    editor.setStrokeColor(color);
-  }, [color]);
+  // useEffect(() => {
+  //   if (!editor || !fabric) {
+  //     return;
+  //   }
+  //   editor.canvas.freeDrawingBrush.color = color;
+  //   editor.setStrokeColor(color);
+  // }, [color]);
 
-  const toggleDraw = () => {
-    editor.canvas.isDrawingMode = !editor.canvas.isDrawingMode;
-  };
-  const undo = () => {
-    if (editor.canvas._objects.length > 0) {
-      history.push(editor.canvas._objects.pop());
-    }
-    editor.canvas.renderAll();
-  };
-  const redo = () => {
-    if (history.length > 0) {
-      editor.canvas.add(history.pop());
-    }
-  };
+  // const toggleDraw = () => {
+  //   editor.canvas.isDrawingMode = !editor.canvas.isDrawingMode;
+  // };
+  // const undo = () => {
+  //   if (editor.canvas._objects.length > 0) {
+  //     history.push(editor.canvas._objects.pop());
+  //   }
+  //   editor.canvas.renderAll();
+  // };
+  // const redo = () => {
+  //   if (history.length > 0) {
+  //     editor.canvas.add(history.pop());
+  //   }
+  // };
 
-  const clear = () => {
-    editor.canvas._objects.splice(0, editor.canvas._objects.length);
-    history.splice(0, history.length);
-    editor.canvas.renderAll();
-  };
+  // const clear = () => {
+  //   editor.canvas._objects.splice(0, editor.canvas._objects.length);
+  //   history.splice(0, history.length);
+  //   editor.canvas.renderAll();
+  // };
 
-  const removeSelectedObject = () => {
-    editor.canvas.remove(editor.canvas.getActiveObject());
-  };
+  // const removeSelectedObject = () => {
+  //   editor.canvas.remove(editor.canvas.getActiveObject());
+  // };
 
-  const onAddCircle = () => {
-    editor.addCircle();
-    editor.addLine();
-  };
-  const onAddRectangle = () => {
-    editor.addRectangle();
-  };
+  // const onAddCircle = () => {
+  //   editor.addCircle();
+  //   editor.addLine();
+  // };
+  // const onAddRectangle = () => {
+  //   editor.addRectangle();
+  // };
   const appendText = (text) => {
     editor.addText(text);
   };
@@ -190,10 +190,10 @@ const useEditor = ({ wrapperRef, imageFile }) => {
     }
     return link.current;
   };
-  const exportSVG = () => {
-    const svg = editor.canvas.toSVG();
-    console.info(svg);
-  };
+  // const exportSVG = () => {
+  //   const svg = editor.canvas.toSVG();
+  //   console.info(svg);
+  // };
   const exportImage = () => {
     editor.canvas.discardActiveObject();
     setTimeout(() => {
